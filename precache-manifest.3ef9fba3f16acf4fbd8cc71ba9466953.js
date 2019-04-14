2c7392cf8e6b705e961c5d9e0b51aa714ad2a808
Jay Li <jay.li23697@gmail.com>
2019-04-15 01:17:31 +0800

    Updates

---
+++ precache-manifest.3ef9fba3f16acf4fbd8cc71ba9466953.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0784d0c2ca0e74577a686d1a8cab3fb0",
+    "url": "/starwars/static/media/Wallpaper.0784d0c2.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "9cd712b42b70d05ab420",
+    "url": "/starwars/static/js/runtime~main.f4b6364c.js"
+  },
+  {
+    "revision": "9f528040be5f85e6d022",
+    "url": "/starwars/static/js/main.b12e1a11.chunk.js"
+  },
+  {
+    "revision": "98ed7cdc48412ba97a74",
+    "url": "/starwars/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "9f528040be5f85e6d022",
+    "url": "/starwars/static/css/main.33dd977c.chunk.css"
+  },
+  {
+    "revision": "98ed7cdc48412ba97a74",
+    "url": "/starwars/static/css/2.fc945419.chunk.css"
+  },
+  {
+    "revision": "8e4d74dbb9db4d6a6e29ec670485e18b",
+    "url": "/starwars/index.html"
+  }
+];
