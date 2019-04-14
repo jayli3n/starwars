63ce1e9987359838a85974d2b5a9e16825206811
Jay Li <jay.li23697@gmail.com>
2019-04-14 19:25:15 +0800

    added function (recursive fetch()) to read API data.

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -19,6 +19,48 @@ class App extends Component {
     console.log(this.state.search_field);
   }
 
+  componentDidMount(){
+    const fetchEverything = (param) => {
+      const fetch2 = (url, data = []) => {
+        return new Promise((resolve, reject) => {
+          fetch(url).then(resp => resp.json()).then(swapi => {
+            data = data.concat(swapi.results);
+            if(swapi.next === null){
+              resolve(data);
+            }
+            else{
+              fetch2(swapi.next, data).then(resolvee => {
+                resolve(resolvee);
+              });
+            }
+          })
+        })
+      }
+      fetch2(param).then(data => {
+        return data;
+      });
+    }
+
+    const loadData = async () => {
+      const resp = await fetch('https://swapi.co/api/');
+      const data = await resp.json();
+      const urls = [
+        data.planets,
+        data.starships,
+        data.vehicles,
+        data.people,
+        data.films,
+        data.species
+      ];
+
+      fetchEverything(urls[0]);
+      // for (let i = 0; i < urls.length; i+=1){
+      //   this.setState({data: this.state.data.concat(fetchEverything(urls[i]))});
+      // }
+    }
+    loadData();
+  }
+
   render() {
     return (
       <div className="tc f4">
