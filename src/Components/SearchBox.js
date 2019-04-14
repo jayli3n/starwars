49f0530b9d731b8959b33ac1b57009672f04c750
Jay Li <jay.li23697@gmail.com>
2019-04-15 01:19:26 +0800

    Changed text

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = (props) => {
 	const {searchChange, total, count, done_loading} = props;
 	const resultsLabel = (count, total) => {
 		if(!done_loading || total > 0){
-			return <h1 id='results_label'>{`Results: ${count}/${total}`}</h1>
+			return <h1 id='results_label'>{`Results Loaded: ${count}/${total}`}</h1>
 		}
 		else if(total === 0 || typeof total === "undefined"){
 			return <h1 id='results_label'>Please select a category.</h1>
